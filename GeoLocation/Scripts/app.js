d68d4c7086fda80b05c07f321e6830c6bd985c7c
FranLsz <franlsz95develop@gmail.com>
2015-10-06 10:42:36 +0200

    final

---
+++ GeoLocation/Scripts/app.js
@@ -0,0 +1,18 @@
+﻿function posicion(pos) {
+    document.getElementById("lat").innerHTML = pos.coords.latitude;
+    document.getElementById("lon").innerHTML = pos.coords.longitude;
+
+
+    //obtencion de imagen desde la api de google
+    var pm = pos.coords.latitude + "," + pos.coords.longitude;
+    var url = "http://maps.googleapis.com/maps/api/staticmap?center=" +
+        pm + "&zoom=16&size=400x400&sensor=false";
+    document.getElementById("mapa").src = url;
+}
+
+if (navigator.geolocation) {
+    //obtencion de permisos para acceder a la ubicacion
+    navigator.geolocation.getCurrentPosition(posicion, function (error) {
+        alert("No puedo acceder a la posicion");
+    });
+}
